a9bb365ad2811ee7f1db0c8d27d1f8bb64956e8d
Aurelien Girona <agirona@z4r8p1.42lyon.fr>
2022-04-04 17:07:38 +0200

    fixed typo

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 14:40:32 by agirona           #+#    #+#             */
-/*   Updated: 2022/03/30 14:59:35 by agirona          ###   ########lyon.fr   */
+/*   Updated: 2022/04/04 16:36:52 by agirona          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int		main(void)
 
 	std::cout << std::endl;
 
-	std::cout << "Address of STR : " << str << std::endl;
-	std::cout << "Address of PTR : " << *stringPTR << std::endl;
-	std::cout << "Address of REF : " << stringREF << std::endl;
+	std::cout << "Value of STR : " << str << std::endl;
+	std::cout << "Value of PTR : " << *stringPTR << std::endl;
+	std::cout << "Value of REF : " << stringREF << std::endl;
 	return (0);
 }
